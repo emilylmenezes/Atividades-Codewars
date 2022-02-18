b69c5f2863c5b71f6e8a2572e3b7a5d2930cf78a
Emily Menezes <84086374+emilylmenezes@users.noreply.github.com>
2022-02-18 11:37:10 -0300

    Atualizando arquivo

--- codeWars.js
+++ codeWars.js
@@ -183,3 +183,21 @@ function withoutLast(arr) {
    return String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33)
  
 };
+
+//Décima oitava questão
+
+function parseInt(string) {
+  // TODO: it's your task now
+  const numerosPorExtenso = {
+  "zero":0, "one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9, 
+  "ten":10, "eleven":11, "twelve":12, "thirteen":13, "fourteen":14, "fifteen":15, "sixteen":16, 
+  "seventeen":17, "eighteen":18, "nineteen":19, "twenty":20, "thirty":30, "forty":40, "fifty":50, 
+  "sixty":60, "seventy":70, "eighty":80, "ninety":90
+};
+const mult = { "hundred":100, "thousand":1000, "million": 1000000 };
+  return string.split(/ |-/).reduce(function(value, palavra) {
+    if (numerosPorExtenso[palavra]) value += numerosPorExtenso[palavra];
+    if (mult[palavra]) value += mult[palavra] * (value % mult[palavra]) - (value % mult[palavra]);
+    return value;
+  },0);
+}
