9404610bb0a2b105b23cb405fbc110e904ca3277
Emily Menezes <84086374+emilylmenezes@users.noreply.github.com>
2022-02-18 11:36:14 -0300

    SUbindo questão

--- codeWars.js
+++ codeWars.js
@@ -175,3 +175,11 @@ function withoutLast(arr) {
               }
               return troqueString  + mudePalavras;
             }
+
+//Décima sétima questão
+    const helloWorld = () => {
+  // feel free to change this into a function :)
+  
+   return String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33)
+ 
+};
